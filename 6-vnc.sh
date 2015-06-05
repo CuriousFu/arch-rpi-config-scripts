8675845ee5f54f7cf58050d2a06986397e95f7f7
Andrew Shamov <lomaster2002@gmail.com>
2015-06-05 22:08:45 +0400

    Update 6-vnc.sh

--- 6-vnc.sh
+++ 6-vnc.sh
@@ -3,3 +3,5 @@ sudo pacman -S tigervnc --noconfirm
 cat vncxstartup >~/.vnc/xstartup
 
 sudo cat vnc-service >/etc/systemd/system/vncserver@:1.service
+sudo systemctl daemon-reload
+sudo systemctl enable vncserver@:1.service
