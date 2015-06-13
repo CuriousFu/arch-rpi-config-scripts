78a40cea7d3ab84a1c7aa56f5613f0726ad742a0
Andrew Shamov <lomaster2002@gmail.com>
2015-06-13 18:52:04 +0400

    Update 4-utils.sh

--- 4-utils.sh
+++ 4-utils.sh
@@ -1,3 +1,3 @@
 echo "Installing utilities packages..."
-pacman -S --noconfirm htop screen mc git unzip unrar p7zip pixz patch
+pacman -S --noconfirm htop screen mc git unzip unrar p7zip pixz
 echo "  done."
