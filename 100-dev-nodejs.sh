1abcdeaa0eca6d937344d1850156ab578a35f7ed
Andrew Shamov <lomaster2002@gmail.com>
2015-06-13 19:41:57 +0400

    Update 100-dev-nodejs.sh

--- 100-dev-nodejs.sh
+++ 100-dev-nodejs.sh
@@ -5,5 +5,6 @@ sudo pacman -S patch make gcc python2 --noconfirm
 npm install git+https://git@github.com/nekuz0r/wiring-pi.git#incoming-node-v0.12.x
 #npm install wiring-pi
 
-sudo npm install -g express
-sudo npm install -g express-generator
+#sudo npm install -g express-generator
+npm install express
+
