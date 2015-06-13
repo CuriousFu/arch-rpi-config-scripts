fcf5c865f528f9cad04447247f32457360b5355a
Andrew Shamov <lomaster2002@gmail.com>
2015-06-13 19:36:16 +0400

    Update and rename 100-dev-node.sh to 100-dev-nodejs.sh

--- 100-dev-node.sh
+++
@@ -1,7 +0,0 @@
-sudo pacman -S nodejs npm --noconfirm
-sudo npm install -g node-gyp
-
-sudo pacman -S patch make gcc python2 --noconfirm
-npm install wiring-pi
-
-
---
+++ 100-dev-nodejs.sh
@@ -0,0 +1,9 @@
+sudo pacman -S nodejs npm --noconfirm
+sudo npm install -g node-gyp
+
+sudo pacman -S patch make gcc python2 --noconfirm
+npm install git+https://git@github.com/nekuz0r/wiring-pi.git#incoming-node-v0.12.x
+#npm install wiring-pi
+
+sudo npm install -g express
+sudo npm install -g express-generator
