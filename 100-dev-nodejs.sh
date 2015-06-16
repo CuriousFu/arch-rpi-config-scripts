1bdccd0cc26828154349ccaf2ea5f239e474aff1
Andrew Shamov <lomaster2002@gmail.com>
2015-06-16 22:02:10 +0400

    Update 100-dev-nodejs.sh

--- 100-dev-nodejs.sh
+++ 100-dev-nodejs.sh
@@ -1,7 +1,9 @@
 sudo pacman -S nodejs npm --noconfirm
+
+sudo pacman -S python2 --noconfirm
 sudo npm install -g node-gyp
 
-sudo pacman -S patch make gcc python2 --noconfirm
+sudo pacman -S patch make gcc --noconfirm
 npm install git+https://git@github.com/nekuz0r/wiring-pi.git#incoming-node-v0.12.x
 #npm install wiring-pi
 
