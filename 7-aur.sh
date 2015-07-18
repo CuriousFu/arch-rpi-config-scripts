8bc394d19b127cdc2a0daf903bde5e98ed6201c7
Andrew Shamov <lomaster2002@gmail.com>
2015-07-18 13:59:43 +0400

    Update 7-aur.sh

--- 7-aur.sh
+++ 7-aur.sh
@@ -1,15 +1,17 @@
 sudo pacman -S --needed --noconfirm base-devel
-sudo pacman -S yajl
+sudo pacman -S --noconfirm yajl
 curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
 tar -xvf package-query.tar.gz
 cd package-query
 makepkg
 sudo pacman -U --noconfirm *.pkg.tar.xz
+cd ..
 
 curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
 tar -xvf yaourt.tar.gz
 cd yaourt
 makepkg
 sudo pacman -U --noconfirm *.pkg.tar.xz
+cd ..
 
 yaourt -Syu --noconfirm
